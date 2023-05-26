1dd4fd220c6d3b2472c69ea674177673d24bec01
Adam Dziomdziora <a.dziomdziora@outlook.com>
2023-05-26 08:33:26 +0200

    cleanup the code

--- AzureBatchHelpers.cs
+++ AzureBatchHelpers.cs
@@ -4,17 +4,18 @@ using Azure.Storage;
 using Microsoft.Azure.Batch.Common;
 using Microsoft.Azure.Batch;
 using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
 
 namespace ConsoleApp1
 {
     internal static class AzureBatchHelpers
     {
-        internal static void CreateBatchPool(BatchClient batchClient, VirtualMachineConfiguration vmConfiguration, IConfigurationRoot config)
+        private static async Task CreateBatchPool(BatchClient batchClient, VirtualMachineConfiguration vmConfiguration, IConfigurationRoot config)
         {
             // Batch resource settings
-            var PoolId = config.GetSection("PoolId").Value;
+            var PoolId = config.GetSection("PoolId").Value ?? "";
             var PoolNodeCount = int.Parse(config.GetSection("PoolNodeCount").Value ?? "2");
-            var PoolVMSize = config.GetSection("PoolVMSize").Value;
+            var PoolVMSize = config.GetSection("PoolVMSize").Value ?? "";
 
             try
             {
@@ -24,7 +25,7 @@ namespace ConsoleApp1
                     virtualMachineSize: PoolVMSize,
                     virtualMachineConfiguration: vmConfiguration);
 
-                pool.Commit();
+                await pool.CommitAsync();
             }
             catch (BatchException be)
             {
@@ -40,14 +41,14 @@ namespace ConsoleApp1
             }
         }
 
-        internal static VirtualMachineConfiguration CreateVirtualMachineConfiguration(ImageReference imageReference)
+        private static VirtualMachineConfiguration CreateVirtualMachineConfiguration(ImageReference imageReference)
         {
             return new VirtualMachineConfiguration(
                 imageReference: imageReference,
                 nodeAgentSkuId: "batch.node.windows amd64");
         }
 
-        internal static ImageReference CreateImageReference(IConfigurationRoot config)
+        private static ImageReference CreateImageReference(IConfigurationRoot config)
         {
             return new ImageReference(
                 publisher: "MicrosoftWindowsServer",
@@ -78,14 +79,14 @@ namespace ConsoleApp1
         /// <param name="containerName">The name of the blob storage container to which the file should be uploaded.</param>
         /// <param name="filePath">The full path to the file to upload to Storage.</param>
         /// <returns>A ResourceFile instance representing the file within blob storage.</returns>
-        internal static ResourceFile UploadFileToContainer(BlobContainerClient containerClient, string containerName, string filePath, string? storedPolicyName = null)
+        private static async Task<ResourceFile> UploadFileToContainer(BlobContainerClient containerClient, string containerName, string filePath, string? storedPolicyName = null)
         {
             Console.WriteLine("Uploading file {0} to container [{1}]...", filePath, containerName);
-            string blobName = Path.GetFileName(filePath);
+            var blobName = Path.GetFileName(filePath);
             filePath = Path.Combine(Environment.CurrentDirectory, filePath);
 
             var blobClient = containerClient.GetBlobClient(blobName);
-            blobClient.Upload(filePath, true);
+            await blobClient.UploadAsync(filePath, true);
 
             // Set the expiry time and permissions for the blob shared access signature. 
             // In this case, no start time is specified, so the shared access signature 
@@ -119,6 +120,157 @@ namespace ConsoleApp1
             {
                 throw new InvalidOperationException("BlobClient must be authorized with shared key credentials to create a service SAS.");
             }
+        }
+
+
+        internal static IEnumerable<CloudTask> CreateTasksForJob(string JobId, IEnumerable<ResourceFile> inputFiles)
+        {
+            // Create a collection to hold the tasks that we'll be adding to the job
+            Console.WriteLine("Adding {0} tasks to job [{1}]...", inputFiles.Count(), JobId);
+            var tasks = new List<CloudTask>();
+
+            // Create each of the tasks to process one of the input files. 
+            foreach (var file in inputFiles)
+            {
+                var taskId = string.Format("Task{0}", tasks.Count);
+                var inputFilename = file.FilePath;
+                var taskCommandLine = string.Format("cmd /c type {0}", inputFilename);
+
+                var task = new CloudTask(taskId, taskCommandLine)
+                {
+                    ResourceFiles = new List<ResourceFile> { file }
+                };
+                tasks.Add(task);
+            }
+
+            return tasks;
+        }
+
+        internal static void PrintTasksOutput(string JobId, BatchClient batchClient)
+        {
+            // Print task output
+            Console.WriteLine();
+            Console.WriteLine("Printing task output...");
+
+            var completedtasks = batchClient.JobOperations.ListTasks(JobId);
+            foreach (var task in completedtasks)
+            {
+                var nodeId = string.Format(task.ComputeNodeInformation.ComputeNodeId);
+                Console.WriteLine("Task: {0}", task.Id);
+                Console.WriteLine("Node: {0}", nodeId);
+                Console.WriteLine("Standard out:");
+                Console.WriteLine(task.GetNodeFile(Constants.StandardOutFileName).ReadAsString());
+            }
+        }
+
+        internal static async Task WaitForTasksCompletion(string? JobId, BatchClient batchClient, IEnumerable<CloudTask> tasks)
+        {
+            // Add all tasks to the job.
+            await batchClient.JobOperations.AddTaskAsync(JobId, tasks);
+
+            // Monitor task success/failure, specifying a maximum amount of time to wait for the tasks to complete.
+            var timeout = TimeSpan.FromMinutes(30);
+            Console.WriteLine("Monitoring all tasks for 'Completed' state, timeout in {0}...", timeout);
+
+            var addedTasks = batchClient.JobOperations.ListTasks(JobId);
+            batchClient.Utilities.CreateTaskStateMonitor().WaitAll(addedTasks, TaskState.Completed, timeout);
+            Console.WriteLine("All tasks reached state Completed.");
+        }
+
+
+        internal static async Task CreateBatchPool(IConfigurationRoot config, string PoolId, BatchClient batchClient)
+        {
+            Console.WriteLine("Creating pool [{0}]...", PoolId);
+
+            // Create a Windows Server image, VM configuration, Batch pool
+            var imageReference = CreateImageReference(config);
+            var vmConfiguration = CreateVirtualMachineConfiguration(imageReference);
+            await CreateBatchPool(batchClient, vmConfiguration, config);
+        }
+
+        internal static async Task CreateBatchJob(string JobId, string PoolId, BatchClient batchClient)
+        {
+            // Create a Batch job
+            Console.WriteLine("Creating job [{0}]...", JobId);
+
+            try
+            {
+                var job = batchClient.JobOperations.CreateJob();
+                job.Id = JobId;
+                job.PoolInformation = new PoolInformation { PoolId = PoolId };
+                await job.CommitAsync();
+            }
+            catch (BatchException be)
+            {
+                // Accept the specific error code JobExists as that is expected if the job already exists
+                if (be.RequestInformation?.BatchError?.Code == BatchErrorCodeStrings.JobExists)
+                {
+                    Console.WriteLine("The job {0} already existed when we tried to create it", JobId);
+                }
+                else
+                {
+                    throw; // Any other exception is unexpected
+                }
+            }
+        }
+
+        internal static async Task CleanUpStorage(string InputContainerName, BlobContainerClient containerClient)
+        {
+
+            // Clean up Storage resources
+            await containerClient.DeleteIfExistsAsync();
+            Console.WriteLine("Container [{0}] deleted.", InputContainerName);
+        }
+
+        internal static async Task CleanUpJob(string JobId, BatchClient batchClient)
+        {
+            // Clean up Batch resources (if the user so chooses)
+            Console.WriteLine();
+            Console.Write("Delete job? [yes] no: ");
+            var response = Console.ReadLine()?.ToLower();
+            if (response != "n" && response != "no")
+            {
+                await batchClient.JobOperations.DeleteJobAsync(JobId);
+            }
+        }
+
+        internal static async Task CleanUpTasks(string PoolId, BatchClient batchClient)
+        {
+            Console.Write("Delete pool? [yes] no: ");
+            var response = Console.ReadLine()?.ToLower();
+            if (response != "n" && response != "no")
+            {
+                await batchClient.PoolOperations.DeletePoolAsync(PoolId);
+            }
+        }
+
+        internal static void PrintTotalOperationTime(Stopwatch timer)
+        {
+            // Print out some timing info
+            timer.Stop();
+            Console.WriteLine();
+            Console.WriteLine("Sample end: {0}", DateTime.Now);
+            Console.WriteLine("Elapsed time: {0}", timer.Elapsed);
+        }
+
+        internal static async Task<IEnumerable<ResourceFile>> UploadInputFiles(string InputContainerName, BlobContainerClient containerClient)
+        {
+            var inputFiles = new List<ResourceFile>();
+            // The collection of data files that are to be processed by the tasks
+            var inputFilePaths = new List<string>
+                {
+                    "taskdata0.txt",
+                    "taskdata1.txt",
+                    "taskdata2.txt"
+                };
+
+            // Upload the data files to Azure Storage. This is the data that will be processed by each of the tasks that are
+            // executed on the compute nodes within the pool.
+            foreach (var filePath in inputFilePaths)
+            {
+                inputFiles.Add(await UploadFileToContainer(containerClient, InputContainerName, filePath));
+            }
+            return inputFiles;
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -1,30 +1,24 @@
 ﻿using ConsoleApp1;
 using Microsoft.Azure.Batch;
 using Microsoft.Azure.Batch.Auth;
-using Microsoft.Azure.Batch.Common;
 using Microsoft.Extensions.Configuration;
 using System.Diagnostics;
 
-Console.WriteLine("Hello, World!");
-
 var configuration = new ConfigurationBuilder()
         .AddJsonFile($"appsettings.json");
-
 var config = configuration.Build();
+
 // Update the Batch and Storage account credential strings below with the values unique to your accounts.
-// These are used when constructing connection strings for the Batch and Storage client objects.
-
 // Batch account credentials
-var BatchAccountName = config.GetSection("BatchAccountName").Value;
-var BatchAccountKey = config.GetSection("BatchAccountKey").Value;
-var BatchAccountUrl = config.GetSection("BatchAccountUrl").Value;
-
+var BatchAccountName = config.GetSection("BatchAccountName").Value ?? "";
+var BatchAccountKey = config.GetSection("BatchAccountKey").Value ?? "";
+var BatchAccountUrl = config.GetSection("BatchAccountUrl").Value ?? "";
 // Storage account credentials
-var StorageAccountName = config.GetSection("StorageAccountName").Value;
-var StorageAccountKey = config.GetSection("StorageAccountKey").Value;
-
-var JobId = config.GetSection("JobId").Value; 
-var PoolId = config.GetSection("PoolId").Value;
+var StorageAccountName = config.GetSection("StorageAccountName").Value ?? "";
+var StorageAccountKey = config.GetSection("StorageAccountKey").Value ?? "";
+var JobId = config.GetSection("JobId").Value ?? "";
+var PoolId = config.GetSection("PoolId").Value ?? "";
+var InputContainerName = config.GetSection("InputContainerName").Value ?? "";
 
 if (string.IsNullOrEmpty(BatchAccountName) ||
                string.IsNullOrEmpty(BatchAccountKey) ||
@@ -45,130 +39,29 @@ try
     // Create the blob client, for use in obtaining references to blob storage containers
     var blobServiceClient = AzureBatchHelpers.GetBlobServiceClient(StorageAccountName, StorageAccountKey);
 
-    // Use the blob client to create the input container in Azure Storage 
-    const string inputContainerName = "input";
-    var containerClient = blobServiceClient.GetBlobContainerClient(inputContainerName);
-    containerClient.CreateIfNotExistsAsync().Wait();
+    var containerClient = blobServiceClient.GetBlobContainerClient(InputContainerName);
+    await containerClient.CreateIfNotExistsAsync();
 
-    // The collection of data files that are to be processed by the tasks
-    var inputFilePaths = new List<string>
-                {
-                    "taskdata0.txt",
-                    "taskdata1.txt",
-                    "taskdata2.txt"
-                };
-
-    // Upload the data files to Azure Storage. This is the data that will be processed by each of the tasks that are
-    // executed on the compute nodes within the pool.
-    var inputFiles = new List<ResourceFile>();
-
-    foreach (var filePath in inputFilePaths)
-    {
-        inputFiles.Add(AzureBatchHelpers.UploadFileToContainer(containerClient, inputContainerName, filePath));
-    }
+    var inputFiles = await AzureBatchHelpers.UploadInputFiles(InputContainerName, containerClient);
 
     // Get a Batch client using account creds
     var cred = new BatchSharedKeyCredentials(BatchAccountUrl, BatchAccountName, BatchAccountKey);
 
     using var batchClient = BatchClient.Open(cred);
-    Console.WriteLine("Creating pool [{0}]...", PoolId);
+    await AzureBatchHelpers.CreateBatchPool(config, PoolId, batchClient);
+    await AzureBatchHelpers.CreateBatchJob(JobId, PoolId, batchClient);
 
-    // Create a Windows Server image, VM configuration, Batch pool
-    var imageReference = AzureBatchHelpers.CreateImageReference(config);
-    var vmConfiguration = AzureBatchHelpers.CreateVirtualMachineConfiguration(imageReference);
-    AzureBatchHelpers.CreateBatchPool(batchClient, vmConfiguration, config);
+    var tasks = AzureBatchHelpers.CreateTasksForJob(JobId, inputFiles);
 
-    // Create a Batch job
-    Console.WriteLine("Creating job [{0}]...", JobId);
+    await AzureBatchHelpers.WaitForTasksCompletion(JobId, batchClient, tasks);
 
-    try
-    {
-        var job = batchClient.JobOperations.CreateJob();
-        job.Id = JobId;
-        job.PoolInformation = new PoolInformation { PoolId = PoolId };
-        job.Commit();
-    }
-    catch (BatchException be)
-    {
-        // Accept the specific error code JobExists as that is expected if the job already exists
-        if (be.RequestInformation?.BatchError?.Code == BatchErrorCodeStrings.JobExists)
-        {
-            Console.WriteLine("The job {0} already existed when we tried to create it", JobId);
-        }
-        else
-        {
-            throw; // Any other exception is unexpected
-        }
-    }
+    AzureBatchHelpers.PrintTasksOutput(JobId, batchClient);
 
-    // Create a collection to hold the tasks that we'll be adding to the job
-    Console.WriteLine("Adding {0} tasks to job [{1}]...", inputFiles.Count, JobId);
-    var tasks = new List<CloudTask>();
+    AzureBatchHelpers.PrintTotalOperationTime(timer);
 
-    // Create each of the tasks to process one of the input files. 
-    for (int i = 0; i < inputFiles.Count; i++)
-    {
-        var taskId = string.Format("Task{0}", i);
-        var inputFilename = inputFiles[i].FilePath;
-        var taskCommandLine = string.Format("cmd /c type {0} && mkdir C:\\test2 && echo 213 >> C:\\test2\\2.txt", inputFilename);
-
-        var task = new CloudTask(taskId, taskCommandLine)
-        {
-            ResourceFiles = new List<ResourceFile> { inputFiles[i] }
-        };
-        tasks.Add(task);
-    }
-
-    // Add all tasks to the job.
-    batchClient.JobOperations.AddTask(JobId, tasks);
-
-    // Monitor task success/failure, specifying a maximum amount of time to wait for the tasks to complete.
-    var timeout = TimeSpan.FromMinutes(30);
-    Console.WriteLine("Monitoring all tasks for 'Completed' state, timeout in {0}...", timeout);
-
-    var addedTasks = batchClient.JobOperations.ListTasks(JobId);
-    batchClient.Utilities.CreateTaskStateMonitor().WaitAll(addedTasks, TaskState.Completed, timeout);
-    Console.WriteLine("All tasks reached state Completed.");
-
-    // Print task output
-    Console.WriteLine();
-    Console.WriteLine("Printing task output...");
-
-    var completedtasks = batchClient.JobOperations.ListTasks(JobId);
-    foreach (CloudTask task in completedtasks)
-    {
-        string nodeId = string.Format(task.ComputeNodeInformation.ComputeNodeId);
-        Console.WriteLine("Task: {0}", task.Id);
-        Console.WriteLine("Node: {0}", nodeId);
-        Console.WriteLine("Standard out:");
-        Console.WriteLine(task.GetNodeFile(Constants.StandardOutFileName).ReadAsString());
-    }
-
-    // Print out some timing info
-    timer.Stop();
-    Console.WriteLine();
-    Console.WriteLine("Sample end: {0}", DateTime.Now);
-    Console.WriteLine("Elapsed time: {0}", timer.Elapsed);
-
-    // Clean up Storage resources
-    containerClient.DeleteIfExistsAsync().Wait();
-    Console.WriteLine("Container [{0}] deleted.", inputContainerName);
-
-    // Clean up Batch resources (if the user so chooses)
-    Console.WriteLine();
-    Console.Write("Delete job? [yes] no: ");
-    var response = Console.ReadLine()?.ToLower();
-    if (response != "n" && response != "no")
-    {
-        batchClient.JobOperations.DeleteJob(JobId);
-    }
-
-    Console.Write("Delete pool? [yes] no: ");
-    response = Console.ReadLine()?.ToLower();
-    if (response != "n" && response != "no")
-    {
-        batchClient.PoolOperations.DeletePool(PoolId);
-    }
+    await AzureBatchHelpers.CleanUpStorage(InputContainerName, containerClient);
+    await AzureBatchHelpers.CleanUpJob(JobId, batchClient);
+    await AzureBatchHelpers.CleanUpTasks(PoolId, batchClient);
 }
 finally
 {
